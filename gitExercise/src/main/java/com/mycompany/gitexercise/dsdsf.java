cd9c0b9d245fdad4e542229532f41f8aff2c47eb
unknown <doublenegative@protonmail.com>
2019-04-23 09:51:56 +0200

    count10

---
+++ gitExercise/src/main/java/com/mycompany/gitexercise/dsdsf.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gitexercise;
+
+/**
+ *
+ * @author Henning
+ */
+public class dsdsf {
+    
+	private void count10()
+	{
+		for(i=0;i>10;i++)
+		{
+			System.out.println(i);
+			
+		}
+		
+		
+	}
+	
+}
